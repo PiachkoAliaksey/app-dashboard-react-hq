2fb1c158d58b5bcef9e7ff57df388d60adf0e3f3
PiachkoAliaksey <a.pechko@agima.ru>
2025-02-07 17:16:16 +0300

    refactor files

--- src/components/BarFooter/CardFooter.tsx
+++ src/components/BarFooter/CardFooter.tsx
@@ -2,7 +2,7 @@ import { TCardFooter } from '../../types'
 
 const CardFooter = ({ title, icon, currentCard, setCurrentCard }:TCardFooter) => {
     return (
-        <div onClick={() => setCurrentCard(title)} className={`relative flex flex-col gap-1 items-center opacity-50 ${currentCard === title && 'opacity-100'}`}>
+        <div onClick={() => setCurrentCard(title)} className={`relative flex flex-col gap-1 items-center opacity-50 cursor-pointer ${currentCard === title && 'opacity-100'}`}>
             <div className="relative flex items-center">
                 {icon}
                 {title === 'Profile' && <span className="absolute  top-[-8px] right-[-8px] w-5  h-5 bg-lime-500 rounded-full flex items-center justify-center text-[14px]">3</span>}
