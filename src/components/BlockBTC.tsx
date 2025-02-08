91158f0ad186cf9076f08694d084d06bc895d1ad
PiachkoAliaksey <a.pechko@agima.ru>
2025-02-08 15:27:22 +0300

    refactor files

--- src/components/BlockBTC.tsx
+++ src/components/BlockBTC.tsx
@@ -1,4 +1,5 @@
 import data from '../data/data.min.json';
+import imageCoin from '../assets/hcoin.png'
 
 const BlockBTC = () => {
     return (
@@ -9,11 +10,11 @@ const BlockBTC = () => {
                 <div className='w-1/2 flex flex-col gap-0.5 md:gap-2'>
                     <div className='flex gap-2 items-center justify-end'>
                         <span className='font-bold text-[12px] uppercase opacity-50 md:text-[18px]'>Balance:</span>
-                        <p className='text-[14px] font-bold text-white md:text-[20px]'>{data.balance}</p>
+                        <p className='text-[14px] font-bold text-white flex items-center gap-1 md:text-[20px]'>{data.balance} <img src={imageCoin} className='w-4 h-4' alt='image'/></p>
                     </div>
                     <div className='flex gap-2 items-center justify-end'>
                         <span className='font-bold text-[12px] uppercase opacity-50 md:text-[18px]'>On hold:</span>
-                        <p className='text-[14px] font-bold text-white md:text-[20px]'>{data.on_hold}</p>
+                        <p className='text-[14px] font-bold text-white flex items-center gap-1 md:text-[20px]'>{data.on_hold} <img src={imageCoin} className='w-4 h-4' alt='image'/></p>
                     </div>
                 </div>
             </div>
--- src/components/SwitchBar.tsx
+++ src/components/SwitchBar.tsx
@@ -8,7 +8,7 @@ const SwitchBar = ({ currentInterval, handlerClickInterval }: TSwitchBar) => {
 
     return (
         <div className='w-full mt-4 h-fit px-4 flex gap-2 items-center'>
-            <span className='opacity-50'>Time Range:</span>
+            <span className='opacity-50 text-[12px] md:text-xl '>Time Range:</span>
             {dataInterval.map((item) => (
                 <div key={item} onClick={() => handlerClickInterval(mapDataInterval[item])} className={`px-2 py-1 rounded-xl w-[53px] border-2 text-[10px] opacity-50 flex items-center justify-center cursor-pointer transition-all ${currentInterval === mapDataInterval[item] ? 'opacity-100' : ''}`}>{item}</div>
             ))}
